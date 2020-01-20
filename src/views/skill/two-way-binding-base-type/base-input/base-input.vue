85444781925e2f148a4f86d1b43b870f1a1f45b8
Fifth-Patient <1923407743@qq.com>
2020-01-20 14:22:43 +0800

    skill: two-way-binding-base-type

---
+++ src/views/skill/two-way-binding-base-type/base-input/base-input.vue
@@ -0,0 +1,28 @@
+<template>
+  <input type="text" v-model="fieldValue">
+</template>
+
+<script>
+export default {
+  name: 'base-input',
+  props: {
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      fieldValue: this.value // 初始化赋值
+    }
+  },
+  watch: {
+    value (newVal) {
+      this.fieldValue = newVal // 监听props（外部）更新，赋值给data（内部）
+    },
+    fieldValue () {
+      this.$emit('input', this.fieldValue) // data（内部）更新，抛出到外部
+    }
+  }
+}
+</script>
