a44d503f2ba156bbccaa0c7c192c3134881a6d5a
Fifth-Patient <1923407743@qq.com>
2020-01-24 20:03:11 +0800

    skill: emitter

---
+++ src/views/skill/emitter/child2.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="skill-emitter-child2">
+    {{ selfContent }} <button @click="handleClick">reload</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'skill-emitter-child2',
+  props: {
+    content: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      selfContent: this.content
+    }
+  },
+  methods: {
+    handleClick () {
+      this.selfContent = this.content
+    },
+    handleChange ($event) {
+      this.selfContent = $event
+    }
+  },
+  created () {
+    this.$on('on-change', this.handleChange)
+  }
+}
+</script>
---
+++ src/views/skill/emitter/child3.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="skill-emitter-child3"></div>
+</template>
+
+<script>
+export default {
+  name: 'skill-emitter-child3',
+  methods: {
+    handleChange ($event) {
+      console.log($event)
+    }
+  },
+  created () {
+    this.$on('on-change', this.handleChange)
+  }
+}
+</script>
