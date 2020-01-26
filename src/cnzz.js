097ecaee4972a9fe0616aff7a2d2b18e08211594
Fifth-Patient <1923407743@qq.com>
2020-01-27 00:04:23 +0800

    config: cnzz

---
+++ src/cnzz.js
@@ -0,0 +1,21 @@
+const cnzz = {
+  src: process.env.VUE_APP_CNZZ,
+  render () {
+    console.log(process.env)
+    const NODE_ENV = process.env.NODE_ENV
+    if (NODE_ENV === 'production') {
+      const body = document.body
+      const div = document.createElement('div')
+      const script = document.createElement('script')
+      
+      div.classList.add('none')
+      script.type = 'text/javascript'
+      script.src = this.src
+
+      div.insertBefore(script, div.lastChild)
+      body.insertBefore(div, body.lastChild)
+    }
+  }  
+}
+
+export default cnzz
--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import title from './libs/title'
+import cnzz from './cnzz'
+
+cnzz.render()
 
 Vue.config.productionTip = false
 
