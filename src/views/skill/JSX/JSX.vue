c83acd5d87ac09dd29f2a3e0295a0392375d37c6
Fifth-Patient <1923407743@qq.com>
2020-01-28 14:09:58 +0800

    skill: JSX of *.js & *.vue

---
+++ src/views/skill/JSX/JSX.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: "skill-jsx-vue",
+  data() {
+    return {
+      type: "vue"
+    };
+  },
+  render() {
+    return (
+      <p>
+        hello
+        <b> {this.type}</b>
+      </p>
+    )
+  }
+}
+</script>
