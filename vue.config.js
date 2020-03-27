c88a6af3c8ce0b2cd62653a1b47f494e1f6afaee
Fifth-Patient <1923407743@qq.com>
2020-03-28 02:58:09 +0800

    global: .env.development

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,7 @@ const path = require('path')
 
 module.exports = {
   // runtimeCompiler: true, // only need runtime
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/stardust/'
-    : '/',
+  publicPath: process.VUE_APP_PUBLIC_PATH,
   configureWebpack: {
     devtool: 'source-map'
   },
