0338442ac46e0c48ef7d09c0e508b4ec766ffd53
Fifth-Patient <1923407743@qq.com>
2020-04-12 00:51:00 +0800

    global: depoly shell of git commit message about time and commit id

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,27 @@
+# 定义变量
+time=$(date "+%Y-%m-%d %H:%M:%S")
+current_commit_id=`git rev-parse --short HEAD`
+
+# 重置dist目录
+rm -rf dist
+mkdir dist
+
+# cd 到构输出的目录下 
+cd dist
+
+# clone 指定远端分支
+git  clone -b gh-pages https://github.com/Fifth-Patient/stardust.git
+
 # 构建
 npm run build
 
-# cd 到构建输出的目录下 
-cd dist
-
 # git commit
-git init
 git add -A
-git commit -m 'deploy'
+git commit -m "deploy size by commit id $current_commit_id at $time"
+
+# 拉取github的gh-pages分支
+git pull origin gh-pages
 
 # 部署到github的gh-pages分支
-git push -f git@github.com:Fifth-Patient/stardust.git master:gh-pages
+git push git@github.com:Fifth-Patient/stardust.git master:gh-pages
+
