55c5e76402d180671b8890b961a49c0b36b7f37f
Fifth-Patient <1923407743@qq.com>
2020-04-12 10:53:24 +0800

    Optimized deployment script mechanism

--- deploy.sh
+++ deploy.sh
@@ -4,24 +4,24 @@ current_commit_id=`git rev-parse --short HEAD`
 
 # 重置dist目录
 rm -rf dist
-mkdir dist
-
-# cd 到构输出的目录下 
-cd dist
-
-# clone 指定远端分支
-git  clone -b gh-pages https://github.com/Fifth-Patient/stardust.git
-
-# 构建
-npm run build
-
-# git commit
-git add -A
-git commit -m "deploy size by commit id $current_commit_id at $time"
+echo remove dist
 
 # 拉取github的gh-pages分支
-git pull origin gh-pages
+git clone -b gh-pages https://github.com/Fifth-Patient/stardust.git/ dist
+
+# 构建（不清除dist目录）
+npm run build_no_clean
+
+
+# git commit
+cd dist
+git add -A
+git commit -m "deploy size by commit id $current_commit_id at $time"
+echo deploy size by commit id $current_commit_id at $time
+
+# 以变基方式拉取github的gh-pages分支
+git pull https://github.com/Fifth-Patient/stardust.git/ gh-pages --rebase
 
 # 部署到github的gh-pages分支
-git push git@github.com:Fifth-Patient/stardust.git master:gh-pages
+git push https://github.com/Fifth-Patient/stardust.git/ gh-pages:gh-pages
 
