24d5ef5ffe12773530e701d3d8e4f223fa98f16e
Fifth-Patient <1923407743@qq.com>
2020-04-12 11:27:44 +0800

    deploy shell use shh remote ssh link

--- deploy.sh
+++ deploy.sh
@@ -6,12 +6,11 @@ current_commit_id=`git rev-parse --short HEAD`
 rm -rf dist
 echo remove dist
 
-# 拉取github的gh-pages分支
-git clone -b gh-pages https://github.com/Fifth-Patient/stardust.git/ dist
+# 拉取github的gh-pages分支到指定目录
+git clone -b gh-pages git@github.com:Fifth-Patient/stardust.git dist
 
 # 构建（不清除dist目录）
 npm run build_no_clean
-
 
 # git commit
 cd dist
@@ -20,8 +19,8 @@ git commit -m "deploy size by commit id $current_commit_id at $time"
 echo deploy size by commit id $current_commit_id at $time
 
 # 以变基方式拉取github的gh-pages分支
-git pull https://github.com/Fifth-Patient/stardust.git/ gh-pages --rebase
+git pull git@github.com:Fifth-Patient/stardust.git gh-pages --rebase
 
 # 部署到github的gh-pages分支
-git push https://github.com/Fifth-Patient/stardust.git/ gh-pages:gh-pages
+git push git@github.com:Fifth-Patient/stardust.git gh-pages:gh-pages
 
