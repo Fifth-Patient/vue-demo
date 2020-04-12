b9e0e86ecc67a331d7ee93207f8b44142a559d50
Fifth-Patient <1923407743@qq.com>
2020-04-12 10:54:02 +0800

    Revert "deploy size by commit id 122beb1 at 2020-04-12 01:15:02"
    This reverts commit 03c3ab042d0b37b162e1b22c58745726856e4f20.

--- js/chunk-2d237b50.58e3781a.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d237b50"],{fbec:function(t,e,c){"use strict";c.r(e);var n=function(){var t=this,e=t.$createElement,c=t._self._c||e;return c("iframe",{attrs:{src:t.mountHtmlSrc,frameborder:"0"}})},r=[],l=(c("99af"),{name:"skill-lifecycyle-mount",computed:{mountHtmlSrc:function(){var t=this.$route.query.type;return"".concat("/stardust/","skill-lifecycle-").concat(t,".html")}}}),u=l,a=c("2877"),o=Object(a["a"])(u,n,r,!1,null,null,null);e["default"]=o.exports}}]);
-//# sourceMappingURL=chunk-2d237b50.58e3781a.js.map
